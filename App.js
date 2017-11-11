688ab99c2e47e11dd842cf111af91a010847acc9
Bderiel <bderiel@gmail.com>
2017-11-11 16:18:25 -0500

    fix(cant destructure components for some reason)

--- App.js
+++ App.js
@@ -1,31 +1,16 @@
-import ShowAllPath from './components/ShowAllPath';
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { Platform, Text, View } from 'react-native';
-import { fetchMastries } from './redux/masteries';
 import store from './redux';
 import { Provider } from 'react-redux';
+import  Home  from './components/Home';
 
-
-class App extends Component {
-  componentDidMount() {
-    this.props.initialData();
-  }
+export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <ShowAllPath />
+        <Home />
       </Provider>
     );
   }
 }
 
-const mapProps = null;
-
-const mapDispatchtoProps = dispatch => ({
-  initialData: () => {
-    dispatch(fetchMastries());
-  },
-});
-
-export default connect(mapProps, mapDispatchtoProps)(App);
