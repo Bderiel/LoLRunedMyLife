13de68ab40eef5b2b7261e9a8ca39c210b2fefea
Pat <Pat@Pat.local>
2017-11-11 18:04:56 -0500

    got props

--- components/Dominance.js
+++ components/Dominance.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 // import Button from 'react-native-button';
 // import { Actions } from 'react-native-router-flux';
 
-export default class Dominance extends Component {
+class Dominance extends Component {
   render() {
+    console.log(this.props)
     return (
       <Text>Hello world!</Text>
     );
   }
 }
+
+const mapProps = state => ({ masteries: state.masteries });
+
+export default connect(mapProps, {})(Dominance);
