cf37dfd802dc9ce8b3fd61da0d6386d7cb122c61
Bderiel <bderiel@gmail.com>
2017-11-11 15:01:28 -0500

    redux store

--- App.js
+++ App.js
@@ -1,25 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 import ShowAllPath from './components/ShowAllPath';
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { connect } from 'react-redux';
+import { Platform,Text,View } from 'react-native';
+import { fetchMastries } from './redux/masteries'
+import store from './store'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
+class App extends Component<{}> {
+  componentDidMount(){
+    this.props.initialData();
+  }
   render() {
     return (
       <ShowAllPath/>
@@ -27,21 +16,12 @@ export default class App extends Component<{}> {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+const mapProps = null;
+
+const mapDispatchtoProps = dispatch => ({
+  initialData: () => {
+    dispatch(fetchMastries());
   },
 });
+
+export default connect(mapProps, mapDispatchtoProps)(App);
---
+++ redux/masteries.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const GET_MASTERIES = 'GET_MASTERIES';
+
+const getMasteries = masteries => ({ type: GET_MASTERIES, masteries });
+
+export default function (state = [], action) {
+  switch (action.type) {
+    case GET_MASTERIES:
+      return action.masteries;
+    default:
+      return state;
+  }
+}
+
+export const fetchMasteries = () =>
+  dispatch =>
+    axios.get('http://ddragon.leagueoflegends.com/cdn/7.22.1/data/en_US/mastery.json')
+      .then(res =>
+        dispatch(getMasteries(res.data)))
+      .catch(err => console.log(err));
