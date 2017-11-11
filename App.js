89d08094c925468f2299f99e90a5cca4b93b7367
Pat <Pat@Pat.local>
2017-11-11 17:12:39 -0500

    Components rendering

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import { Platform, Text, View } from 'react-native';
-import store from './redux';
-import { Provider } from 'react-redux';
-import  Home  from './components/Home';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import ShowAllPath from './components/ShowAllPath';
 
 export default class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Home />
-      </Provider>
+      <ShowAllPath />
     );
   }
 }
+
+
 
