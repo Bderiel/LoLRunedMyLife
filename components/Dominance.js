ef2557fe531f917a33fd47917f09983bd2799065
Bderiel <bderiel@gmail.com>
2017-11-12 17:10:42 -0500

    feat single-champ view

--- components/Dominance.js
+++ components/Dominance.js
@@ -1,36 +1,43 @@
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
-// import Button from 'react-native-button';
+import styles from '../StyleSheet';
+import Button from 'react-native-button';
+import { Actions } from 'react-native-router-flux';
 // import { Actions } from 'react-native-router-flux';
 
 class Dominance extends Component {
   render() {
     const champs = this.props.champions.data;
-    const keys = Object.keys(champs);
-    const arr = keys.map(character => champs[character]);
-    console.log(arr);
+   const keys = Object.keys(champs);
+  const arr = keys.map(character => champs[character]);
     return (
-    <View>
-      {arr.map(char=>{
-        return(
-          <View>
-          <Text key={char.id}>{char.id}</Text>
+      <ScrollView >
+        {arr.map((char) => (
+          <View key={char.id} style={styles.squares}>
+            <Button onPress={Actions.LandingPage}>
+          <Text>{char.id}</Text>
           <Image
             style={{ width: 50, height: 50 }}
-              source={{ uri: 'http://pluspng.com/img-png/league-of-legends-png-league-of-legends-png-image-png-image-200.png' }}
+              source={{
+                uri: `http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${char.id}.png 
+` }}
           />
+            </Button>
           </View>
-        )
-      })}
-    </View>
-    )
+        ))}
+      </ScrollView>
+    );
   }
 }
 
 const mapProps = state => ({
- masteries: state.masteries,
+  masteries: state.masteries,
   champions: state.champions,
 });
 
 export default connect(mapProps, {})(Dominance);
+
+  //<Button onPress={Actions.Dominance}>Dominance</Button>;
+
+     // <TouchableOpacity onPress={() => { Actions.Job({ jobId: jobId }) }}>
