4bb0eb07b0832916241e00ca774d26f2dbcdfd1a
PatPetillo <patpetillo@gmail.com>
2017-11-13 12:51:09 -0500

    styles added

---
+++ components/AllChampions.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Text, View, Image, ScrollView } from 'react-native';
+import { connect } from 'react-redux';
+import styles from '../StyleSheet';
+import Button from 'react-native-button';
+import { Actions } from 'react-native-router-flux';
+
+class AllChampions extends Component {
+  render() {
+    const champs = this.props.champions.data;
+    const keys = Object.keys(champs);
+    const arr = keys.map(character => champs[character]);
+    return (
+      <ScrollView >
+        <View style={ styles.allChampions }>
+          {arr.map(char => (
+            <View key={char.id} style={{ padding: 10, alignItems: 'center' }}>
+              <Button onPress={() => { Actions.SingleChamp({ chamId: char.id, champ:char, title:char.id }) }}>
+                <Image
+                  style={{ width: 75, height: 75, borderRadius: 50, }}
+                  source={{
+                  uri: `http://ddragon.leagueoflegends.com/cdn/7.22.1/img/champion/${char.id}.png`
+                }}/>
+              </Button>
+              <Text style={{color:"white"}}>{char.id}</Text>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+const mapProps = state => ({
+  masteries: state.masteries,
+  champions: state.champions,
+});
+
+export default connect(mapProps, {})(AllChampions);
--- components/SingleChamp.js
+++ components/SingleChamp.js
@@ -13,16 +13,16 @@ export default class SingleChamp extends Component {
   render() {
     const champ = this.props.champ;
     return (
-      <View>
-          <Text>{champ.id}</Text>
-          <Text>{champ.title}</Text>
-          <Text>{champ.blurb}</Text>
-            <Image
-                style={{ width: 200, height: 200 }}
-                source={{
-                    uri: `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champ.id}_0.jpg
-`,
-                }}/>
+      <View style={ styles.singleChampion }>
+        <View style={{ alignItems: 'center', margin: 10 }}>
+          <Text style={{color: 'red', fontSize: 20 }}>{champ.id}</Text>
+
+          <Text style={{color: 'white', fontSize: 16}}>{champ.title.slice(3)}</Text>
+
+          <Image style={{ width: 250, height: 250 }} source={{uri: `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champ.id}_0.jpg`}} />
+
+          <Text style={{color: 'grey'}}>{champ.blurb}</Text>
+        </View>
       </View>
     );
   }
