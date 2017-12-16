d1e99abbd778b6a618f4e7cb5ad0cac35f151cac
Bderiel <bderiel@gmail.com>
2017-12-15 23:43:28 -0500

    to merge

--- components/Runes.js
+++ components/Runes.js
@@ -4,6 +4,7 @@ import { Text, View, Image, ScrollView } from 'react-native';
 
 export default class Runes extends Component {
     render() {
+        console.log(this.props.runes)
         return (
             <View >
            <Text>TEST</Text>
