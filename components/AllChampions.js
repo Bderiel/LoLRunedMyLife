b21428565b46cb4c69bc1b5434cb5ad34b8db5a7
PatPetillo <patpetillo@gmail.com>
2017-12-18 11:03:02 -0500

    a few updates and a bunch of code clean up

--- components/AllChampions.js
+++ components/AllChampions.js
@@ -1,36 +1,37 @@
 import React, { Component } from 'react';
 import { Text, View, Image, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
-import styles from '../StyleSheet';
+import PropTypes from 'prop-types';
 import Button from 'react-native-button';
 import { Actions } from 'react-native-router-flux';
+import styles from '../StyleSheet';
 
-class AllChampions extends Component {
-  render() {
-    const champs = this.props.champions.data;
-    const keys = Object.keys(champs);
-    const arr = keys.map(character => champs[character]);
-    return (
-      <ScrollView >
-        <View style={ styles.allChampions }>
-          {arr.map(char => (
-            <View key={char.id} style={{ padding: 10, alignItems: 'center' }}>
-              <Button onPress={() => { Actions.SingleChamp({ chamId: char.id, champ:char, title:char.id }) }}>
-                <Image
-                  style={{ width: 75, height: 75, borderRadius: 50 }}
-                  defaultSource={require('../images/ring.png')}
-                  source={{
-                  uri: `http://ddragon.leagueoflegends.com/cdn/7.22.1/img/champion/${char.id}.png`
-                }}/>
-              </Button>
-              <Text style={{color:"white"}}>{char.id}</Text>
-            </View>
-          ))}
-        </View>
-      </ScrollView>
-    );
-  }
-}
+
+const AllChampions = (props) => {
+  const champs = props.champions.data;
+  const keys = Object.keys(champs);
+  const arr = keys.map(character => champs[character]);
+  return (
+    <ScrollView >
+      <View style={ styles.allChampions }>
+        {arr.map(char => (
+          <View key={char.id} style={{ padding: 10, alignItems: 'center' }}>
+            <Button onPress={() => { Actions.SingleChamp({ chamId: char.id, champ: char, title: char.id }) }}>
+              <Image
+                style={{ width: 75, height: 75, borderRadius: 50 }}
+                defaultSource={require('../images/ring.png')}
+                source={{
+                uri: `http://ddragon.leagueoflegends.com/cdn/7.22.1/img/champion/${char.id}.png`
+              }}/>
+            </Button>
+            <Text style={{ color: 'white' }}>{char.id}</Text>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
+
 
 const mapProps = state => ({
   masteries: state.masteries,
@@ -38,3 +39,7 @@ const mapProps = state => ({
 });
 
 export default connect(mapProps, {})(AllChampions);
+
+AllChampions.propTypes = {
+  champions: PropTypes.objectOf(PropTypes.any).isRequired,
+};
--- components/Runes.js
+++ components/Runes.js
@@ -1,38 +1,38 @@
 import React, { Component } from 'react';
 import { Text, View, Image, ScrollView } from 'react-native';
+import PropTypes from 'prop-types';
 import styles from '../StyleSheet';
 
+const Runes = (props) => {
+  return (
+    <ScrollView >
+      <View style={{ backgroundColor: 'black' }}>
+        {props.runes.map((el, idx) => {
+          let cat = '';
+          if (idx === 0) cat = 'Keystone';
+          if (idx === 1) cat = 'Greater Runes';
+          if (idx === 2) cat = 'Lesser Runes Set 1';
+          if (idx === 3) cat = 'Lesser Runes Set 2';
+          return (
+            <View key={idx} style={{ alignItems: 'center', margin: 20 }}>
+              <Text style={styles.header}>{cat}</Text>
+              {el.runes.map(rune => (
+                <View key={rune.id} style={{ alignItems: 'center', margin: 20 }}>
+                  <Text style={{ color: 'red', fontSize: 20 }}>{rune.name}</Text>
+                  <Image style={{ width: 100, height: 100 }} source={{ uri: `http://media.services.zam.com/v1/media/byName/${rune.name}.png?width=50`}} />
+                  <Text style={{ color: 'grey' }}>{rune.shortDesc}</Text>
+                </View>
+              ))}
+            </View>
+          );
+        })}
+      </View>
+    </ScrollView>
+  );
+};
 
-export default class Runes extends Component {
-  render() {
-    console.log(this.props.runes);
-    return (
-      <ScrollView >
-            <View style={{ backgroundColor: 'black' }}>
-        {this.props.runes.map((el, idx) => {
-                let cat = '';
-                if (idx === 0) cat = 'Keystone';
-                if (idx === 1) cat = 'Greater Runes';
-                if (idx === 2) cat = 'Lesser Runes Set 1';
-                if (idx === 3) cat = 'Lesser Runes Set 2';
-               return (
-                   <View key={idx} style={{ alignItems: 'center', margin: 20 }}>
-                       <Text style={styles.header}>{cat}</Text>
-                   {el.runes.map(rune => (
-                           <View key={rune.name}style={{ alignItems: 'center', margin: 20 }}>
-                               <Text style={{ color: 'red', fontSize: 20 }}>{rune.name}</Text>
-                               <Image style={{ width: 100, height: 100 }} source={{
-                                   uri: `http://media.services.zam.com/v1/media/byName/${rune.name}.png?width=50`}} />
-                               <Text style={{ color: 'grey' }}>{rune.shortDesc}</Text>
+export default Runes;
 
-                     </View>
-                           ))}
-                 </View>
-               );
-           })}
-           </View>
-      </ScrollView>
-    );
-  }
-}
-
+Runes.propTypes = {
+  runes: PropTypes.arrayOf(PropTypes.any).isRequired,
+};
