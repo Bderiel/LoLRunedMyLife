de6dd4d7fc5c5d723aa3d52604f95657d1dd114f
PatPetillo <patpetillo@gmail.com>
2017-12-18 11:03:02 -0500

    a few updates and a bunch of code clean up

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -4,17 +4,17 @@ import Button from 'react-native-button';
 import { Actions } from 'react-native-router-flux';
 import styles from '../StyleSheet';
 
-export default class LandingPage extends Component {
-  render() {
-    const remote = 'https://news-a.akamaihd.net/public/images/misc/GameBox.jpg'
-    return (
-      <ImageBackground source={{ uri: remote }} style={ styles.backgroundImage }>
-        <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', padding: 150}}>
-          <Button style={ styles.button } onPress={Actions.AllChampions}>All Champions</Button>
-          <Button style={ styles.button } onPress={Actions.Masteries}>LoL Runes Runed My Life</Button>
-          <Button style={styles.button} onPress={Actions.Search}>Match History</Button>
-        </View>
-      </ImageBackground>
-    )
-  }
-}
+const LandingPage = () => {
+  const remote = 'https://news-a.akamaihd.net/public/images/misc/GameBox.jpg'
+  return (
+    <ImageBackground source={{ uri: remote }} style={ styles.backgroundImage }>
+      <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', padding: 150}}>
+        <Button style={ styles.button } onPress={Actions.AllChampions}>All Champions</Button>
+        <Button style={ styles.button } onPress={Actions.Masteries}>LoL Runes Runed My Life</Button>
+        <Button style={styles.button} onPress={Actions.Search}>Match History</Button>
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default LandingPage;
--- components/Masteries.js
+++ components/Masteries.js
@@ -3,32 +3,27 @@ import { Text, View, Image, ScrollView } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 import Button from 'react-native-button';
+import PropTypes from 'prop-types';
 import styles from '../StyleSheet';
 
-class Masteries extends Component {
-  render() {
-    return (
-        <ScrollView >{this.props.masteries.length && this.props.masteries.map(perkStyle=>{
-            return(
-                <View key={perkStyle.id} style={{ flex: 1, alignItems: 'center', backgroundColor:'black'}}>
-            <View style={{ flexDirection:'row'}}>
-                <Button onPress={() => {Actions.Runes({title: perkStyle.name, runes:perkStyle.slots})}}>
-                    <View style={{ flexDirection: 'column', alignItems: 'center' }}>
-                                <Text style={{ color: "red" }}>{perkStyle.name.toUpperCase()}</Text>
-                        <Text style={{ color:"grey" }}>Improved Attacks</Text>
-                        <Text style={{ color:"grey" }}>Sustained Damage</Text>
-                    </View>
-                            <Image style={{ width: 200, height: 200 }} source={{ uri: `http://media.services.zam.com/v1/media/byName/lolking/img/runes/${perkStyle.name.toLowerCase()}/icon.png`}}/>
-                </Button>
+const Masteries = props => (
+  <ScrollView >
+    {props.masteries.length && props.masteries.map(perkStyle => (
+      <View key={perkStyle.id} style={{ flex: 1, alignItems: 'center', backgroundColor: 'black' }}>
+        <View style={{ flexDirection: 'row' }}>
+          <Button onPress={() => { Actions.Runes({ title: perkStyle.name, runes: perkStyle.slots }) ;}}>
+            <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+              <Text style={{ color: 'red' }}>{perkStyle.name.toUpperCase()}</Text>
+              <Text style={{ color: 'grey' }}>Improved Attacks</Text>
+              <Text style={{ color: 'grey' }}>Sustained Damage</Text>
             </View>
-            </View>
-            )
-            })}
-          
-        </ScrollView>
-    );
-  }
-}
+            <Image style={{ width: 200, height: 200 }} source={{ uri: `http://media.services.zam.com/v1/media/byName/lolking/img/runes/${perkStyle.name.toLowerCase()}/icon.png` }} />
+          </Button>
+        </View>
+      </View>
+    ))}
+  </ScrollView>
+);
 
 
 const mapProps = state => ({
@@ -36,3 +31,7 @@ const mapProps = state => ({
 });
 
 export default connect(mapProps, {})(Masteries);
+
+Masteries.propTypes = {
+  masteries: PropTypes.arrayOf(PropTypes.any).isRequired,
+};
